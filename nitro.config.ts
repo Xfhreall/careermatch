16c9abfdddcaf12f387c04b378a38e04a27a4de1
Xfhreall <farelrizqi377@gmail.com>
2026-05-16 21:12:33 +0700

    fix: deploy config environments

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,4 +3,7 @@ import { defineNitroConfig } from "nitro/config"
 export default defineNitroConfig({
   preset: "cloudflare-module",
   compatibilityDate: "2026-05-16",
+  cloudflare: {
+    deployConfig: false,
+  },
 })
