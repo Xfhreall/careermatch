9fdc513efbbcdbcc90f67a22d157f53bd911e123
Xfhreall <farelrizqi377@gmail.com>
2026-05-19 02:39:57 +0700

    chore: update nitro config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,9 @@
-import { defineNitroConfig } from "nitro/config"
+import { defineNitroConfig } from "nitro/config";
 
 export default defineNitroConfig({
   preset: "cloudflare-module",
   compatibilityDate: "2026-05-16",
   cloudflare: {
-    deployConfig: false,
+    deployConfig: true,
   },
-})
+});
